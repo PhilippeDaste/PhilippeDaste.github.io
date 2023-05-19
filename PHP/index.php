41ae6a4e51f89b821c97ace5823ac76bb81fc912
PhilippeDaste <philippe.daste@oclock.school>
2023-05-19 12:10:27 +0200

    pas réussi à capter le CSS pour footer et nav

--- PHP/index.php
+++ PHP/index.php
@@ -2,8 +2,8 @@
     // J'inclus le nav
     include "../Templates/nav.php";
 
-	// J'inclus ma page index.php
-	// include "../PHP/index.php";
+	// J'inclus le head
+	include "../Templates/head.php";
 
 
     // J'inclus le footer
@@ -13,27 +13,7 @@
 <!DOCTYPE html>
 <html>
 
-<head>
-	<meta charset="utf-8">
-	<!-- meta est une balise orpheline, elle permet d'insérer un élément à un endroit précis-->
-	<link href="https://fonts.bunny.net/css?family=cabin:400,500,500i" rel="stylesheet" />
-	
-	<!--Liaison avec mon fichier CSS sake-style-->
-	<link rel="stylesheet" href="sake_style.css" />
-	
-	<!--Je récupère ici une police asiatique de IBM sur Googlefont : -->
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+JP:wght@300&display=swap" rel="stylesheet">
-    
-	<!--Ici je récupère la police Dosis de Google font-->
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Dosis:wght@300&family=IBM+Plex+Sans+JP:wght@300&family=Piazzolla:wght@300&display=swap" rel="stylesheet">
-	
-	<title>J'apprends avec Openclassroom</title>
-	
-</head>
+
 
 
 <!-- body contient TOUT ce qui sera affiché à l'écran -->
--- Templates/footer.php
+++ Templates/footer.php
@@ -1,11 +1,19 @@
+<?php
+
+	// J'inclus le head pour avoir la mise en forme CSS
+	include "../Templates/head.php";
+
+?>
+
+
 </main>
 
 
 <!--Pied de page-->
 	<footer class="conteneur_footer">
 		Ce site est public et également très personnel.
-		Vous pouvez essayer de me joindre à <span><a class="footer_link_color" href="mailto:philippe.daste@oclock.school">Mail O'Clock</a> </span> mais n'espérez pas trop une réponse.
-		
+		Vous pouvez essayer de me joindre à  philippe.daste@oclock.school  mais n'espérez pas trop une réponse.
+
 	
 	</footer>
 
