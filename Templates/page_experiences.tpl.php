172e903657de3b2ce7172d53a49595da83952d4d
PhilippeDaste <philippe.daste@oclock.school>
2023-05-21 21:01:24 +0200

    dernier commit dimanche mis grid dans le premier article

--- Templates/page_experiences.tpl.php
+++ Templates/page_experiences.tpl.php
@@ -4,10 +4,35 @@
 	
     <p>De quoi faire vivre irrégulièrement le site</p>
 
-    <h2 id="mon_ancre">Une rencontre fortuite : bodaimoto 菩提酛</h2>
-<!-- le id="" ne peut être mis que sur une balise, pas un mot ?? -->
-	<p class="paragraph_experience">C'est à Nara, dans une petite boutique près de la gare, sur le conseil du gérant (grognon) que j'ai bu du sake Mimurosugi 三諸杉 de la maison Imanishi.
+    <section class="conteneur_section">
 
-	</p>
-    <a href="https://imanishisyuzou.com/" target="_blank">La maison Imanishi de Nara</a>
+        <h2 class="one" >Réception pour l'anniversaire de l'empereur </h2>
+	    <p class="two" > Jusqu'à récemment mes activités avec l'ambassade du Japon me permettaient d'être invité à la réception annuelle à l'occasion de l'anniversaire de l'empereur.<br>
+            Point notable supplémentaire, notre anniversaire tombait les même jour et mois, le 23 décembre.
+            Changement avec le nouvel empereur, ce n'est plus le jour anniversaire que nous avons en commun mais le jour et l'année de naissance.
+            Ce dernier étant né en début d'année, celà permet de faire la réception par un temps plus clément et ainsi de profiter du jardin.
+            Très belle réception encore cette année, Pierre Hermé était invité et à été décoré par M.l'Ambassadeur.
+            Plein de bonnes choses et, bien sûr, quelques sake a déguster.
+            Je vous reparlerais du Mizubasho, dont j'ai un peu abusé, du Asamurasaki, sans odeur mais d'une belle couleur orange/rosé, et du Kagatori.<br>
+            Mais le sujet du jour c'est bien le Shichikanba 七冠馬.<br>
+            La chose intéressante est que le logo de cette marque ressemble à un fer à cheval (teitetsu 蹄鉄) car cette marque est liée à une course de chevaux (kyousouba 競走馬).
+            Ce que j'ai appris le lendemain en me rendant sur le site.
+            Annecdote, comme la réception se terminait et que je discutais encore avec la dame qui présentait ce sake elle m'a donné la bouteille entammée.
+        </p>
+
+        <img class="three" class="position_image" src="../images_sake/shichikanba_recadré.png" alt="logo shichikanba" width=" 200px" height="200px" >
+
+        <a class="four" class="position_lien" href="https://sake-hikami.co.jp/page-info/" target="_blank">La brasserie Ikami 簸上</a>    
+
+    </section>
+
+
+    <section class="conteneur-section">
+        <h2 class="paragraph_experience">Une rencontre fortuite : bodaimoto 菩提酛 </h2>
+	    <p>C'est à Nara, dans une petite boutique près de la gare, sur le conseil du gérant (grognon) que j'ai bu du sake Mimurosugi 三諸杉 de la maison Imanishi.</p>
+        <a class="position_lien" href="https://imanishisyuzou.com/" target="_blank">La maison Imanishi 今西 de Nara</a>  
+    </section>
+ 
+
+   
 </body>
--- Templates/partials/head-content.php
+++ Templates/partials/head-content.php
@@ -1,6 +1,7 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <!-- meta est une balise orpheline, elle permet d'insérer un élément à un endroit précis-->
+
 <!--Liaison avec mon fichier CSS sake-style-->
 <link rel="stylesheet" href="../CSS/sake_style.css" />
 	
