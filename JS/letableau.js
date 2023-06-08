4680f3604b826eca52d7fff5d44cfbcde1f75cad
PhilippeDaste <philippe.daste@oclock.school>
2023-06-08 17:40:48 +0200

    rajouté une ligne au tableau et changé style. reste un doute avec place des src

--- JS/letableau.js
+++ JS/letableau.js
@@ -4,18 +4,40 @@
 
 /* TODO
 1) créer un tableau[] contenant les nouvelles informations à importer
-2) créer une nouvelle ligne vide dans mon Tableau sur page_marques
-3) remplir le la nouvelle ligne dans le Tableau html
-4) créer une fonction permettant de faire les opérations.
+2) je vais chercher l'endroit ou je veux créer une nouvelle ligne : le Tableau dans ma page html
+3) créer une nouvelle ligne vide dans mon Tableau sur page_marques
+4) remplir/coller la nouvelle ligne (les td) dans le Tableau html 
+5) créer une fonction permettant de faire les opérations.
+*/
 
-BONUS
+//1) créer tableau avec nouveaux éléments, les éléments du tableau sont de même type :texte
+const novelBewery = ["Hyogo 兵庫", "Takara 宝", "兵庫県松戸市新作字高田111 〒271-0052","Shirakabekura 白壁蔵"];
+
+//2) et 3)
+let myTableBody = document.querySelector("tbody"); // je sélectionne un endroit
+const newLine = document.createElement("tr"); // je crée l'argument
+newLine.setAttribute("id","4"); // méthode pour modifier un argument, ici je crée un id
+myTableBody.append(newLine); // j'insère le nouvel élément dans la page A LA SUITE DES AUTRES (en queue)
+console.log(myTableBody);
+//YES  !!!!! créé un tr avec id="4" à l'endroit que je voulais dans la table !!!
+// 3)
+
+//4) presque comme le petit tailleur ??
+let myTr = document.getElementById("4");
+for(let i=0 ; i< novelBewery.length ; i++) {
+    nouveauTd = document.createElement("td");
+    nouveauTd.textContent = novelBewery[i];
+    myTr.append(nouveauTd)
+} 
+
+// 5) créer la fonction ET le module
+
+/*BONUS
 créer une colonne "Remarques" avec des cases de taille variable pour pouvoir ensuite ajouter des commentaires, ou pas.
 */
 
-// les éléments du tableau sont de même type :texte
-const novelBewery = ["Hyogo 兵庫", "Takara 宝", "兵庫県松戸市新作字高田111 〒271-0052","Shirakabekura 白壁蔵"];
 
-/* Je crée une nouvelle ligne <tr> , dois-je avoir des <td> ?? Logiquement céee 1 tr puis 4 td.
-et je ma met à la fin du tableau happend () */
-const myTable = document.querySelector("table");
-console.log(myTable);
+
+
+
+
--- Templates/page_marques.tpl.php
+++ Templates/page_marques.tpl.php
@@ -14,7 +14,7 @@
 
         <section>
             <!-- je met un tableau avec les informations de base sur les différentes brasserie-->
-            <table>
+            <table class="brasseriesTable">
                 <caption>Liste des brasseries testées</caption>
                 <tr>
                     <th>Préfecture 県</th>
@@ -22,17 +22,17 @@
                     <th>Adresse 住所</th>
                     <th>Marque(s) 銘柄</th>
                 </tr>
-               
+ 
                 <tr>
-                    <td>Asahi 旭</td>
                     <td>Yamaguchi 山口</td>
+                    <td>Asahi 旭</td>
                     <td>〒742-0422 山口県岩国市周東町獺越2167-4 </td>
                     <td>Dassai 獺祭</td>
                 </tr>
 
                 <tr>
-                    <td>Dewasakura 出羽桜</td>
                     <td>Yamagata 山形</td>
+                    <td>Dewasakura 出羽桜</td>
                     <td>〒994-0044 山形県天童市一日町一丁目4番6号 </td>
                     <td>Dewasakura 出羽桜</td>
                 </tr>
@@ -45,11 +45,12 @@
                 </tr>
 
 
-
             </table>
 
         </section>
 
     </main>
-	
+    <script src="../JS/letableau.js"></script>
+	<script src="../JS/chgttitre.js"></script>
+    
 </body>
