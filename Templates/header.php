f414c9594dd745088885ee40f355ccff463c1306
PhilippeDaste <philippe.daste@oclock.school>
2023-06-16 16:11:40 +0200

    remis de l'ordre ds html et complété ce repo avec les images

--- Templates/header.php
+++ Templates/header.php
@@ -4,5 +4,3 @@
 		<h1>Les sake du Japon 日本酒</h1>
 		<!-- <img src="../images_sake/istockphoto.jpg" alt="riz tasse お猪口 et sake" title="riz, 合, tasse お猪口 et sake"> -->
 	</header>
-
-		<!-- <img src="../images_sake/istockphoto.jpg" alt="riz tasse お猪口 et sake" title="riz, 合, tasse お猪口 et sake"> -->
